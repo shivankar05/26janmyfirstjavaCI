7f9c4b42a96fe9d27c49a40e791609bfd08e2069
Shivankar sahani <shivankarsahani33@gmail.com>
2026-01-29 13:38:49 +0530

    Update output message for August 15 facts

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("========================================");
-        System.out.println("    FACTS ABOUT 15 AUGUST");
+        System.out.println("    THE FACTS ABOUT 15 AUGUST");
         System.out.println("========================================\n");
         
         System.out.println("1. Independence Day of India");
